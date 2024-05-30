c582fc222311d0a1762b71f853d10be149af28d2
alangan17 <alangan17@outlook.com>
2024-05-30 14:44:43 -0700

    change runner start script when using conda

--- start_github_action_runner.bat
+++ start_github_action_runner.bat
@@ -6,7 +6,12 @@ call .\script\conda\00_start_venv_miniconda_v1.bat
 
 @REM Start the GitHub Actions Self-Hosted Runner
 cd %GITHUB_ACTION_RUN_PATH%
-call %GITHUB_ACTION_RUN_PATH%\run.cmd --token %RUNNER_TOKEN%
+@REM call %GITHUB_ACTION_RUN_PATH%\run.cmd --token %RUNNER_TOKEN%
+
+REM Use PowerShell for the setup:
+REM Follow the steps in https://github.com/organizations/GatewayDW/settings/actions/runners/new?arch=x64&os=win
+REM And configure the runner
+call %GITHUB_ACTION_RUN_PATH%\run.cmd
 
 REM Keep the window open to see the output (remove this line if running in background is preferred)
 pause
