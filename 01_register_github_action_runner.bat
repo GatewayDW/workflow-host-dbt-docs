76214dd530433499fe674d4d07cba9d15e60bddd
alangan17 <alangan17@outlook.com>
2025-08-15 16:33:07 -0700

    Remove deprecated GitHub Actions runner scripts and add new versions for registration, starting, and removal

--- 01_register_github_action_runner.bat
+++ 01_register_github_action_runner.bat
@@ -8,21 +8,34 @@ set CURRENT_DIR=%cd%
 call .\script\99_set_env_base.bat
 call .\script\conda\00_start_venv_miniconda_v1.bat
 
-@REM Remove existing runner
+echo "Check if all necessary environment variables are set..."
+set VARS=GITHUB_REPO_URL RUNNER_TOKEN ACTION_RUNNER_DOWNLOAD_URL GITHUB_ACTION_RUN_PATH
+for %%V in (%VARS%) do (
+  if not defined %%V (
+    echo "%%V is not set or empty."
+    exit /b 1
+  )
+)
+echo "All necessary environment variables are set."
+
+echo "Remove existing GitHub Action Runner folder..."
 if exist %GITHUB_ACTION_RUN_PATH% (
     rmdir /s /q %GITHUB_ACTION_RUN_PATH%
 )
 
-@REM Download and Unzip the latest GitHub Actions Self-Hosted Runner
+echo "Downloading and Unzipping the latest GitHub Actions Self-Hosted Runner..."
 mkdir %GITHUB_ACTION_RUN_PATH%
 cd %GITHUB_ACTION_RUN_PATH%
 curl -o action-runner-win-extract.zip -L %ACTION_RUNNER_DOWNLOAD_URL%
 tar -xzf action-runner-win-extract.zip
 
-@REM Remove the zip file
+echo "Cleaning up..."
 del action-runner-win-extract.zip
 
-@REM Start the GitHub Actions Self-Hosted Runner
+echo "Start configuring the GitHub Actions Self-Hosted Runner..."
+echo "Once the configuration is done, you can run the below scripts:"
+echo "  To start the runner service, Execute 02_register_github_action_runner.bat"
+echo "  To remove the runner, Execute 03_remove_github_action_runner.bat"
 @REM Be aware that the questions inside config.cmd is changed
 @REM Use `config.cmd --url %GITHUB_REPO_URL% --token %RUNNER_TOKEN%` if you don't want input redirection
 
@@ -34,3 +47,4 @@ del action-runner-win-extract.zip
 @REM   Enter name of work folder: [press Enter for _work] <ENTER>
 @REM   Would you like to run this runner as a service? (Y/N) [press Enter for N] n
 config.cmd --url %GITHUB_REPO_URL% --token %RUNNER_TOKEN% < %CURRENT_DIR%\answers_runner_setup.txt
+
